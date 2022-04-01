c219175576764cd6d7d51eb50305e2ab7a25ef35
maxschemann <schemann@phonetix.net>
2022-04-01 17:57:00 +0200

    Menü wip

--- Woche2/Tag5/Bestellsystem/src/main/java/ShopService.java
+++ Woche2/Tag5/Bestellsystem/src/main/java/ShopService.java
@@ -4,46 +4,56 @@ import java.util.Scanner;
 
 public class ShopService {
     public static void main(String[] args) {
-
-
-mainMenu();
-if (mainMenu()==1) {
-    System.out.println(listAllProducts());
-    System.out.println("Bitte Produkt auswählen.");
-    Scanner scan = new Scanner(System.in);
-    String id = scan.nextLine();
-    System.out.println(getProductByID(id));
-}
+        int selection = mainMenu();
+        if (selection == 1) {
+            System.out.println("listAllProducts()");}
+        else if (selection==2) System.out.println("listAllOrders");
+        else {
+            System.out.println("Bitte wiederholen Sie die Eingabe");
+            selection = mainMenu();
+        }
 
     }
-    public static int mainMenu() {
 
+    public static int mainMenu() {
         System.out.println("Bitte auswählen: [1] Produkte [2] Bestellungen");
         Scanner scan = new Scanner(System.in);
-        int selection = 0;
-        try {
-            selection = scan.nextInt();
-            Exception wrongNumber = new ArrayIndexOutOfBoundsException("Bitte [1] oder [2] auswählen");
-            if (selection<1 || selection>2) throw wrongNumber;
-        } catch (Exception e) {
-            System.out.println(e);
+        int selection=0;
+        try {selection = scan.nextInt();}
+        catch (Exception e){
+            System.out.println("Ungültige Eingabe");
         }
-        return selection;
-    }
+        System.out.println(selection);
+        return selection;}
+/*
+    public static boolean checkSelection(){
+        try {
+            //Exception wrongNumber = new ArrayIndexOutOfBoundsException();
+            if (selection < 1 || selection > 2) throw new Exception();//wrongNumber;
+        }
+        catch (Exception e) {
+            System.out.println("Bitte [1] oder [2] eingeben" +e);
+        }
+*/
 
-    public static HashMap<String, Product> listAllProducts(){
+
+
+    public static HashMap<String, Product> listAllProducts() {
         ProductRepo products = new ProductRepo();
         return products.listAllProducts();
     }
-    public static Product getProductByID (String id){
+
+    public static Product getProductByID(String id) {
         ProductRepo products = new ProductRepo();
         return products.getProductByID(id);
     }
-    public static HashMap<String, ArrayList<Product>> listAllOrders () {
+
+    public static HashMap<String, ArrayList<Product>> listAllOrders() {
         OrderRepo orders = new OrderRepo();
         return orders.listAllOrders();
     }
-    public static ArrayList<Product> getOrderByID (String id){
+
+    public static ArrayList<Product> getOrderByID(String id) {
         OrderRepo orders = new OrderRepo();
         return orders.getOrderByID(id);
     }
