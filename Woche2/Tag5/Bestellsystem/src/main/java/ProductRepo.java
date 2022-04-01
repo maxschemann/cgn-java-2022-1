5816f9ebebfaac757ac179fadf8a337cb552c848
maxschemann <schemann@phonetix.net>
2022-04-01 10:30:04 +0200

    ProductRepo Test fertig

--- Woche2/Tag5/Bestellsystem/src/main/java/ProductRepo.java
+++ Woche2/Tag5/Bestellsystem/src/main/java/ProductRepo.java
@@ -2,10 +2,19 @@ import java.util.HashMap;
 
 public class ProductRepo {
     HashMap<String, Product> products = new HashMap<>();
-    HashMap<String, Product> listAllProducts (){
+
+    HashMap<String, Product> listAllProducts() {
         return products;
     }
-    Product getProduct(String id){
 
+    Product getProduct(String id) {
+        return products.get(id);
+    }
+
+    @Override
+    public String toString() {
+        return "ProductRepo{" +
+                "products=" + products +
+                '}';
     }
 }
--- Woche2/Tag5/Bestellsystem/src/test/java/ProductRepoTest.java
+++ Woche2/Tag5/Bestellsystem/src/test/java/ProductRepoTest.java
@@ -6,14 +6,24 @@ class ProductRepoTest {
 
     @org.junit.jupiter.api.Test
     void listAllProducts() {
-        HashMap<String, Product> products = new HashMap<>();
+        HashMap<String, Product> testProducts = new HashMap<>();
         Product testProduct1 = new Product("1", "testName1");
         Product testProduct2 = new Product("2", "testName1");
-        products.put(testProduct1.getId(), testProduct1);
-        products.put(testProduct2.getId(), testProduct2);
+        testProducts.put(testProduct1.getId(), testProduct1);
+        testProducts.put(testProduct2.getId(), testProduct2);
+        ProductRepo productRepo = new ProductRepo();
+        productRepo.products.put(testProduct1.getId(), testProduct1);
+        productRepo.products.put(testProduct2.getId(), testProduct2);
+        assertEquals(testProducts.toString(), productRepo.products.toString());
     }
 
     @org.junit.jupiter.api.Test
     void getProduct() {
+        HashMap<String, Product> testProducts = new HashMap<>();
+        Product testProduct1 = new Product("1", "testName1");
+        testProducts.put(testProduct1.id, testProduct1);
+        ProductRepo productRepo = new ProductRepo();
+        productRepo.products.put(testProduct1.id, testProduct1);
+        assertEquals(testProducts.get(1), productRepo.products.get(1));
     }
 }
